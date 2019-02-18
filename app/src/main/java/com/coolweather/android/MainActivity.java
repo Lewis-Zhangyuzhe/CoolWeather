882274c9955d9c84267a84fb87d8c228b8d4d9ae
y2877zha <y2877zha@uwaterloo.ca>
2019-02-18 18:44:33 -0500

    second

---
+++ app/src/main/java/com/coolweather/android/MainActivity.java
@@ -0,0 +1,15 @@
+package com.coolweather.android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.litepaltest.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
